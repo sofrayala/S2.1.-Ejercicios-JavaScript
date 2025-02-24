2cfebf90ca5182dbbfb567a5ce5c0aba650c3d58
Sofia Ayala <sofiarayala1995@gmail.com>
2025-02-24 10:47:25 +0100

    ej4

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ class Person {
 
 const test1 = new Person("Pedro");
 test1.greet();
+
+const printNumbers = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+};
+
+const test2 = [2, 4, 6, 8, 10, 12];
+printNumbers(test2);
