0b6cc56176e946c6039b9ebc7f13b63b86115be1
Sofia Ayala <sofiarayala1995@gmail.com>
2025-02-24 10:30:05 +0100

    ej3

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const add = (a, b) => a + b;
 
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet = () => {
+    console.log(`Hola, ${this.name}`);
+  };
+}
+
+const test1 = new Person("Pedro");
+test1.greet();
